bd845bbc4ea33f74091e6a0811cdbeecd4495b2a
Yuliya Yoncheva <yuliya.yoncheva@gmail.comgit config --global user.email yuliya.yoncheva@gmail.comgit config --global color.ui autogit config --global core.editor edit>
2015-11-12 09:32:22 -0500

    divisibility example

---
+++ Fun_exe.py
@@ -0,0 +1,26 @@
+
+# coding: utf-8
+
+# In[1]:
+
+def divisibility(number):
+    if number % 15 == 0:
+        print ('An Awesome Wave')
+    elif number % 5 == 0:
+        print ('Wave')
+    elif number % 3 == 0:
+        print ('Awesome')
+    else:
+        print number
+
+
+# In[2]:
+
+for number in range (1,101,1):
+    divisibility(number)
+
+
+# In[ ]:
+
+
+
